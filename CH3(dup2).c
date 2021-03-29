5e76ae1e306ecda142d316e7b3b729f280ed418a
Misbah Naeem <59950655+MisbahNaeem@users.noreply.github.com>
2021-03-29 08:47:40 -0700

    Update CH3(dup2).c

--- CH3(dup2).c
+++ CH3(dup2).c
@@ -1,3 +1,5 @@
+//using input output redirection two numbers are simply multiplied  
+//header files.......
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,15 +7,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int main(void) {
+int main(void)
+{
   int num1, num2, mul;// define random variables 
   
-  int input_fds = open("./input.txt", O_RDONLY);
-  int output_fds = open("./output.txt", O_WRONLY | O_CREAT, S_IRUSR |S_IWUSR);
+  int input_fds = open("./input.txt", O_RDONLY);//ope input file in read-ony form
+  int output_fds = open("./output.txt", O_WRONLY | O_CREAT, S_IRUSR |S_IWUSR);//create and open output files.
   
-  int error_fds = open("./error.txt", O_RDONLY);
+  int error_fds = open("./error.txt", O_RDONLY);//open error file in read only form 
   
-  dup2(input_fds, STDIN_FILENO);
+  dup2(input_fds, STDIN_FILENO);//dup2 function..
   dup2(output_fds, STDOUT_FILENO);
   dup2(error_fds, STDERR_FILENO);//dup2 function
   
