be7005fdacc5523c314de1453579c7931060b426
Misbah Naeem <59950655+MisbahNaeem@users.noreply.github.com>
2021-04-12 11:24:58 +0500

    Update Part(C).c

--- Part(C).c
+++ Part(C).c
@@ -26,5 +26,169 @@ void quit(int);
 void runCommand(char*, char*[]);
 unsigned long getTimestamp();
 
+int main(int argc, char* argv[]) 
+{
+
+  char buffer[MAX_COMMAND_LENGTH];
+ 
+  char* command;
+  char* args[MAX_NUM_ARGS + 2];
+  pid_t child_pid;
+  args[MAX_NUM_ARGS + 1] = NULL;
+
+  ignoreSignal(SIGINT);
+  ignoreSignal(SIGTERM);
+
+  while(1) {
+    while((child_pid = waitpid(-1, NULL, WNOHANG)) > 0) {
+      printf("Background process %d terminated\n", child_pid);
+    }
+
+    printf("minishell>");
+    readCommand(buffer, MAX_COMMAND_LENGTH);
+    parseCommand(buffer, &command, args);
+    if(command == NULL) {
+      continue;
+    }
+
+    if(strcmp(command, "exit") == 0) {
+      quit(0);
+    } else if(strcmp(command, "cd") == 0) {
+      cd(args[1]);
+    } else {
+      runCommand(command, args);
+    }
+  }
+
+  exit(1);
+}
+
+void ignoreSignal(int sig) {
+  if(signal(sig, SIG_IGN) == SIG_ERR) {
+    printf("Failed to install signal handler for signal %d\n", sig);
+    exit(1);
+  }
+}
+
+void resetSignal(int sig) {
+  if(signal(sig, SIG_DFL) == SIG_ERR) {
+    printf("Failed to reset signal handler for signal %d\n", sig);
+    exit(1);
+  }
+}
+
+
+void readCommand(char* buffer, int max_size) {
+  /* holds the length of the string read in the buffer. */
+  size_t ln;
+  /* holds the return value of the fgets call. Used for error detection. */
+  char* return_value = fgets(buffer, max_size, stdin);
+
+  if(return_value == NULL) {
+    quit(1);
+  }
+
+  /* Removes the newline character at the end of the buffer if it is
+     present. */
+  ln = strlen(buffer) - 1;
+  if(buffer[ln] == '\n') {
+    buffer[ln] = '\0';
+  }
+}
+
+
+void parseCommand(char* buffer, char** command, char** args) {
+  /* Holds the loop index. */
+  int i;
+  *command = strtok(buffer, " ");
+  args[0] = *command;
+  for(i = 1; i < MAX_NUM_ARGS + 1; i++) {
+    args[i] = strtok(NULL, " ");
+  }
+}
+
+void cd(char* directory) {
+  if(directory == NULL) {
+    goHome();
+    return;
+  }
+
+  if(chdir(directory) == -1) {
+    fprintf(stderr, "Unable to go to directory %s. Attempting to go to HOME directory\n", directory);
+    goHome();
+  }
+}
+
+
+void goHome() {
+  /* Holds the value of the home environment variable. */
+  char* home = getenv("HOME");
+  if(chdir(home) == -1) {
+    fprintf(stderr, "Unable to go to HOME directory. Possibly not set.\n");
+  }
+}
+
+/* Kills all child processes and exits with the given status. */
+void quit(int status) {
+    kill(0, SIGTERM);
+    exit(status);
+  }
+
+
+void runCommand(char* command, char* args[]) {
+  int background = 0;
+  /* Holds the loop index. */
+  int i;
+  
+  unsigned long startTime;
+  /* Holds the return value of the fork system call. */
+  pid_t pid = fork();
+  if(pid == -1) {
+    fprintf(stderr, "Unable to fork.\n");
+    quit(1);
+  }
+
+  for(i = MAX_NUM_ARGS; i >= 0; i--) {
+    if(args[i] != NULL) {
+      if(strcmp(args[i], "&") == 0) {
+        background = 1;
+        args[i] = NULL;
+      }
+      break;
+    }
+  }
+
+  if(pid == 0) {
+    resetSignal(SIGINT);
+    resetSignal(SIGTERM);
+    execvp(command, args);
+    fprintf(stderr, "Unable to start command: %s\n", command);
+    quit(1);
+  } else if(pid > 0) {
+    startTime = getTimestamp();
+    printf("Started %s process with pid %d\n", background ? "background" : "foreground", pid);
+    if(background) {
+      return;
+    }
+
+    while(waitpid(pid, NULL, 0) == -1);
+    printf("Foreground process %d ended\n", pid);
+    //printf("Wallclock time: %.3f\n", (getTimestamp() - startTime) / 1000.0);
+  }
+}
+
+
+unsigned long getTimestamp() {
+  /* Holds the current time from the gettimeofday call. */
+  struct timeval tv;
+  if(gettimeofday(&tv,NULL) == -1) {
+    /* If we are unable to get the time of day we return 0, as this
+       should not be a fatal error. */
+    fprintf(stderr, "Unable to get current time stamp.\n");
+    return 0;
+  }
+  return 1000000 * tv.tv_sec + tv.tv_usec;
+}
+
 
 
